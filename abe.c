bbaa1821b66a788b0961e41135e7e7f1282f837c
DESKTOP-0Q4PG8K\aisaka toshiki <aisakatoshiki1103@gmail.com>
2017-01-12 17:57:27 +0900

    エルシャダイ

--- abe.c
+++ abe.c
@@ -37,5 +37,8 @@ int main(void)
 	printf("あ☆べ\n");
 	printf("あ☆べ\n");
 	printf("ぬ ま た\n");
+	printf("話をしよう。\n");
+	printf("あれは今から、３６万...\nいや、１万２０００年前の出来事。\n");
+	printf("私にとっては昨日の出来事のようだが.../n君たちにとっては/n/n明日の出来事だ。\n");
 	return 0;
 }
